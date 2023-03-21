81e4135ee570f85a80ade53b070a1f1bc8ec7555
Anjang Pangestu <anjangpangestu2304@gmail.com>
2023-03-21 10:22:19 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Personal Website</title>
+    <link rel="stylesheet" type="text/css" href="styles/style.css"/>
+    <script src="https://kit.fontawesome.com/c8e4d183c2.js" crossorigin="anonymous"></script>   
+</head>
+ 
+<body>
+    <section>
+
+        <nav>
+
+            <a href="#" class="logo">My Personal Portfolio</a>
+
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+
+        <div class="home-page">
+            <div id="home">
+                <p>Hallo Semuanya 👋</p>
+                <p>I&#8217;M Anjang Pangestu S</p>
+                <p>Cloud Engineer & UI/UX Design</p>
+                <p>My hobbies are reading, watching movies, studying, photography, 
+                    <br>listening to music, and playing games.</p>
+                <button class="hire-btn">Hire me</button>
+                <button class="down-cv">Download CV</button>
+            </div>
+        </div>
+
+        <img alt="model" class="model" src="https://storage.googleapis.com/storage-app-profile-project/Anjang_profile.png">  
+    </section>
+ 
+    <div class="portfolio-page">
+        <div id="portfolio">
+            <div class="portfolio-text ">
+                <p>Portfolio</p>
+            </div>
+             
+            <div class="box-container">
+    
+                <div class="box-1">
+                    <span>1</span>
+                    <p class="heading">UI/UX Design</p>
+                    <p class="details">Silahkan lihat seluruh portfolio yang sudah dibuat dibagian UI/UX Design.</p>
+                    <button>Lihat Portfolio</button>
+                </div>
+    
+                <div class="box-2">
+                    <span>2</span>
+                    <p class="heading">Cloud Computing</p>
+                    <p class="details">Silahkan lihat seluruh portfolio yang sudah dibuat dibagian Cloud Computing.</p>
+                    <button>Lihat Portfolio</button>
+                </div>
+    
+                <div class="box-3">
+                    <span>3</span>
+                    <p class="heading">Fotograph</p>
+                    <p class="details">Silahkan lihat seluruh portfolio yang sudah dibuat dibagian Portfolio.</p>
+                    <button>Lihat Portfolio</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="contact-page">
+        <div id="contact">
+            <div class="contact-text">
+                <p>Contact</p>
+            </div>
+            <p>Tertarik Dengan Portfolio Saya?</p>
+            <p>Jika kalian tertarik dengan portfolio saya, silahkan melanjutkan dengan menghubungi salah satu media sosial saya yang terdapat dibawah ini</p>
+    
+            <div class="social-icons">
+                <a href="https://twitter.com/AnjangPangestu1"><i class="fab fa-twitter"></i></a>
+                <a href="https://www.instagram.com/anjang_23/"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.linkedin.com/in/anjang-pangestu-selokaton/"><i class="fab fa-linkedin"></i></a>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <p>Copyright My Personal Website by Anjang Pangestu S</p>
+    </footer>
+</body>
+ 
+</html>
